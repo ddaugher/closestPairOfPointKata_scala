3c891f8cc72d05aed66fea85fa82ac930b9245d4
DJ Daugherty <ddaugher@gmail.com>
2016-10-24 10:30:06 -0400

    refactor

--- src/main/scala/com/example/ClosestPair.scala
+++ src/main/scala/com/example/ClosestPair.scala
@@ -18,24 +18,24 @@ object ClosestPair {
     points.sortBy(point => point.y)
   }
 
+  def calculateDistanceBetweenPoints(left: Point, right: Point): Double = {
+    Pair(left, right).distance
+  }
+
+  def isNotZeroDistance(pair: (Point, Point)): Boolean = {
+    if (pair._1.distance(pair._2) != 0.0) return true
+    false
+  }
+
   def force(points: List[Point]): Option[Double] = {
     val none: Option[Double] = None
     if (points.size < 2) return none
 
     type Pair = (Point, Point)
-    val c: Pair => Double = (t) =>  calculatePairValue(t._1, t._2)
+    val c: Pair => Double = (t) =>  calculateDistanceBetweenPoints(t._1, t._2)
 
     val pairs = for(x <- points; y <- points) yield (x, y)
-    Option[Double](pairs.filter(isNotZero).map(c).min)
-  }
-
-  def calculatePairValue(left: Point, right: Point): Double = {
-    Pair(left, right).distance
-  }
-
-  def isNotZero(pair: (Point, Point)): Boolean = {
-    if (pair._1.distance(pair._2) != 0.0) return true
-    false
+    Option[Double](pairs.filter(isNotZeroDistance).map(c).min)
   }
 
 }
